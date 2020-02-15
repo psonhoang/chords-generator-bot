b30d75111ad024e6097291c919861d41e4ac72e5
Pham Son Hoang <pshoang102@gmail.com>
2020-02-15 13:09:40 -0600

    chage tokens

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	SECRET_TOKEN: 'Im4tWmjvoOMexgOogiiuninff1fSuQ8c'
+	SECRET_TOKEN: 'jwtpass'
 }
+//curl -X GET "localhost:1337/webhook?hub.verify_token=jwtpass&hub.challenge=CHALLENGE_ACCEPTED&hub.mode=subscribe"
