2a79367f406f198f299e03023adcb74994d2f803
Pham Son Hoang <pshoang102@gmail.com>
2020-02-15 14:17:19 -0600

    import request

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const request = require('request');
+
 const config = require('./config');
 
 // Handles messages events
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const request = require('request');
-
 const config = require('./config');
 const helpers = require('./helpers')
 
