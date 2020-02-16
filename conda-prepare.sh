f3fbcf9e2ce5a9d70cd1fc79df5587a8ed1db0be
Nam Anh Dinh <nosafegloff@gmail.com>
2020-02-16 03:22:52 -0600

    updated conda setup script

--- conda-prepare.sh
+++ conda-prepare.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-conda create -n datascience # now inside datascience env
+conda create -n datascience -y # now inside datascience env
 conda activate datascience
-conda install keras
+conda install keras -y
 pip install music21
 conda deactivate # now leaving datascience env
 
-conda create -n audio-to-midi-py27 python=2.7
-conda activate audio-to-midi-py27 # now inside python 2.7 env
-conda install numpy
+conda create -n datascience27 python=2.7 -y
+conda activate datascience27 # now inside python 2.7 env
+conda install numpy -y
 pip install -r ~/components/audio-to-midi/requirements.txt
 conda deactivate
