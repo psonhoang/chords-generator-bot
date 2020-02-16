7ea54eb711946314b541508df7e7fdd245746149
Pham Son Hoang <pshoang102@gmail.com>
2020-02-16 10:48:08 -0600

    fix

--- helpers.js
+++ helpers.js
@@ -64,7 +64,7 @@ function handlePostback(sender_psid, received_postback) {
 		callSendAPI(sender_psid, confirm_response);
 
 		//TODO: Script to generate chords
-		shell.exec("bash ./io/convert-wav-remote.sh " + payload + " " + sender_psid.toString());
+		shell.exec("bash ./io/convert-wav-remote.sh " + payload + " " + sender_psid);
 
 		response = responses.finished(sender_psid);
 		global.users[sender_psid].currentState = 'finished';
