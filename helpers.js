39502497ebb01784739dab258c73aff80a2b9999
Pham Son Hoang <pshoang102@gmail.com>
2020-02-16 09:33:12 -0600

    permission

--- helpers.js
+++ helpers.js
@@ -64,7 +64,7 @@ function handlePostback(sender_psid, received_postback) {
 		callSendAPI(sender_psid, confirm_response);
 
 		//TODO: Script to generate chords
-		shell.exec(`./io/convert-wav.sh ${payload} ${sender_psid}`);
+		shell.exec(`bash ./io/convert-wav.sh ${payload} ${sender_psid}`);
 
 		response = responses.finished(sender_psid);
 		global.users[sender_psid].currentState = 'finished';
