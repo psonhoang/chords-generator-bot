a61a7ea055e1acfa0467273c30b1c16dc492525a
Pham Son Hoang <pshoang102@gmail.com>
2020-02-16 12:21:28 -0600

    ssh fix

---
+++ profile.d/ssh-setup.sh
@@ -0,0 +1,29 @@
+# file: .profile.d/ssh-setup.sh
+
+#!/bin/bash
+echo $0: creating public and private key files
+
+# Create the .ssh directory
+mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
+
+# Create the public and private key files from the environment variables.
+echo "$(cat ~/.ssh/id_rsa.pub)" > ${HOME}/.ssh/heroku_id_rsa.pub
+chmod 644 ${HOME}/.ssh/heroku_id_rsa.pub
+
+# Note use of double quotes, required to preserve newlines
+echo "${cat ~/.ssh/heroku_rsa_id}" > ${HOME}/.ssh/heroku_id_rsa
+chmod 600 ${HOME}/.ssh/heroku_id_rsa
+
+# Preload the known_hosts file  (see "version 2" below)
+
+# Start the SSH tunnel if not already running
+SSH_CMD="ssh namanh@linux.cs.uchicago.edu echo testingtesting"
+
+# PID=`pgrep -f "${SSH_CMD}"`
+# if [ $PID ] ; then
+#     echo $0: tunnel already running on ${PID}
+# else
+#     echo $0 launching tunnel
+#     $SSH_CMD
+# fi
