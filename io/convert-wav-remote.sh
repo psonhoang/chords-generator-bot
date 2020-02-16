c99d54c2e04b81c723e343286dd5902cd9af0797
Nam Anh Dinh <nosafegloff@gmail.com>
2020-02-16 11:28:27 -0600

    fixed script

--- io/convert-wav-remote.sh
+++ io/convert-wav-remote.sh
@@ -2,10 +2,11 @@
 
 WAVURL=$1
 MIDINAME=$2
-SECRET="${SESSSION_SECRET}"
+SECRET="n0gloff@UCHI6"
 SSHPROMPT="assword:"
 
-curl $WAVURL -o input.wav
+echo $WAVURL
+curl -X GET $WAVURL -o input.wav
 
 REMOTECMD="cd ~/Documents/uh2020/uncommonhacks2020/io && ./convert-wav-local.sh input.wav $MIDINAME && curl -F 'file=@./$MIDINAME.mid' https://chords-bot.herokuapp.com/upload && echo UPLOADED OUTPUT"
 echo "local says output uploaded"
@@ -13,3 +14,5 @@ echo "local says output uploaded"
 
 
 $(dirname "$0")/scpsshautologin.exp "$SECRET" input.wav ~/Documents/uh2020/uncommonhacks2020/io namanh@linux.cs.uchicago.edu "$REMOTECMD" "$SSHPROMPT"
+
+
